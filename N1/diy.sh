4eaa49554b2af22746b400bb84c62b1f54fd44db
tmxia <55508473+tmxia@users.noreply.github.com>
2025-04-18 01:02:22 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -14,8 +14,8 @@ rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
 
 # 加入OpenClash核心
-#chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
-#$GITHUB_WORKSPACE/N1/preset-clash-core.sh
+chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+$GITHUB_WORKSPACE/N1/preset-clash-core.sh
 
 echo "
 # 插件
